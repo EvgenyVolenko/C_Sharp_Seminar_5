596e0041ac5b217db6e44931f490cba7d30d1d0f
Evgeny <evolenko@inbox.ru>
2022-12-05 23:45:20 +0300

    Added Task036HW

---
+++ Task036HW_Sum_in_chet/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int digit;
+
+Console.Write("Введите длинну массива: ");
+
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out digit)) break;
+    else Console.Write("Ну просил же ЧИСЛО: ");
+}
+
+int[] array = new int[digit];
+
+FillArray(array);
+Console.WriteLine($"Массив после заполнения [{string.Join(", ", array)}]");
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях = {SumArray(array)}");
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+int SumArray(int[] col)
+{
+    int sum = 0;
+    for (int i = 0; i < col.Length; i++)
+    {
+        if (i % 2 != 0) sum += col[i];
+    }
+    return sum;
+}
