c7941d173aac01db1c82f4aca854e458dff8c89e
Evgeny <evolenko@inbox.ru>
2022-12-06 01:42:26 +0300

    Added Task036 change optim

--- Task036HW_Sum_in_chet/Program.cs
+++ Task036HW_Sum_in_chet/Program.cs
@@ -31,9 +31,9 @@ void FillArray(int[] collection)
 int SumArray(int[] col)
 {
     int sum = 0;
-    for (int i = 0; i < col.Length; i++)
+    for (int i = 1; i < col.Length; i++)
     {
-        if (i % 2 != 0) sum += col[i];
+        if (i % 2 == 1) sum += col[i];
     }
     return sum;
 }
